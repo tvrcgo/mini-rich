41c61c430673c5f4bf14f11c7b5c2ebefca95dff
tvrcgo <tvrcgo@gmail.com>
2019-03-26 23:29:13 +0800

    console addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,8 @@
 import { configure } from '@storybook/react'
+import '@storybook/addon-console'
 
 function loadStories() {
   require('./story.js')
-  // You can require as many stories as you need.
 }
 
 configure(loadStories, module)
